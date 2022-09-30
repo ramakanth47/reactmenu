832543b4334e430bb1378d95000b9c2d02ff0385
ramakanth47 <105962313+ramakanth47@users.noreply.github.com>
2022-09-30 17:28:32 +0530

    Add files via upload

---
+++ precache-manifest.44be0fa3780341475e804cd5c766ee1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b21697439bbd74b711ced160df908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46914aaf3248f7118622",
+    "url": "/static/css/main.29d1536b.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46914aaf3248f7118622",
+    "url": "/static/js/main.5ab90454.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
